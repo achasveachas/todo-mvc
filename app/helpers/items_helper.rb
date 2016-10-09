31d8c6bfabf6fbfaaccbba8d07effcae82386523
Yechiel Kalmenson <chilikkal@gmail.com>
2016-10-09 16:45:10 +0000

    refactored using partials and some helper methods

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,17 @@
 module ItemsHelper
+  def li_class_for_item(item)
+    "completed" if item.complete?
+  end
+
+  def li_for_item(item)
+    content_tag_for :li, item, class: li_class_for_item(item) do
+      yield
+    end
+  end
+
+  def form_for_item_status(item)
+    form_for([item.list, item]) do |f|
+      f.check_box :status, class: "toggle", checked: item.complete?
+    end
+  end
 end
